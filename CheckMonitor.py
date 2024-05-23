5e6682ba0554c55480466775928e0f1f7b359d22
nanchacha <67902762+nanchacha@users.noreply.github.com>
2024-05-23 09:26:51 +0900

    Create CheckMonitor.py
    first commit

---
+++ CheckMonitor.py
@@ -0,0 +1,87 @@
+import requests
+import streamlit as st
+from bs4 import BeautifulSoup
+import time
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# http_proxy = '70.10.15.10:8080'
+# https_proxy = '70.10.15.10:8080'
+
+st.markdown("<h2 style='text-align: center; color: #F95700;'>Monitor Help Me Choose 라이브 현황</h2>", unsafe_allow_html=True)
+
+cntrList = ['IQ_AR', 'IQ_KU', 'GR', 'UK', 'ID', 'MY', 'EG', 'HU', 'LEVANT', 'ES', 'LEVANT_AR', 'HK_EN', 'HK', 'RO', 'SE', 'NO',
+            'DK', 'FI', 'AT', 'CH', 'VN', 'CH_FR',
+            'TW', 'IT', 'DE', 'PH', 'BR', 'CZ', 'SK', 'FR', 'N_AFRICA', 'PT', 'KZ_KZ', 'KZ_RU', 'AU', 'ZA', 'EE', 'LV',
+            'LT', 'CA', 'PY', 'AR', 'SI',
+            'HR', 'AE', 'TH', 'NL', 'BE', 'BE_FR', 'SG', 'CL', 'CO', 'MX', 'CA_FR', 'PL', 'UA', 'PK', 'PE', 'AE_AR',
+            'RU', 'UY', 'SA', 'SA_EN', 'IL', 'NZ', 'IN']
+
+# cntrList = ['au', 'uk']
+
+live_list = []
+
+def statusYn(num):
+    result = False
+    if num == 200:
+        result = True
+    return result
+
+
+def getStatus(cntr):
+    r = requests.get("https://www.samsung.com/{}/monitors/help-me-choose".format(cntr), verify=False)
+    num = -1
+    html = r.content
+    soup = BeautifulSoup(html, 'html.parser')
+    elem = soup.find('h1', class_='find-my-monitor__header-title')
+
+    # print('elem : ', elem.text);
+    try:
+        if elem.text:
+            num = 200
+    except:
+        num = -1
+
+    return num
+
+
+div = round(len(cntrList) / 3)
+sectionA = div
+sectionB = div * 2
+sectionC = len(cntrList)
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    for i in range(sectionA):
+        if (statusYn(getStatus(cntrList[i]))):
+            live_list.append(cntrList[i])
+            st.write("{} :white_check_mark:".format(cntrList[i]))
+        else:
+            st.write("{} :red_circle:".format(cntrList[i]))
+
+with col2:
+    for i in range(sectionA, sectionB):
+        if (statusYn(getStatus(cntrList[i]))):
+            live_list.append(cntrList[i])
+            st.write("{} :white_check_mark:".format(cntrList[i]))
+        else:
+            st.write("{} :red_circle:".format(cntrList[i]))
+
+with col3:
+    for i in range(sectionB, len(cntrList)):
+        if (statusYn(getStatus(cntrList[i]))):
+            live_list.append(cntrList[i])
+            st.write("{} :white_check_mark:".format(cntrList[i]))
+        else:
+            st.write("{} :red_circle:".format(cntrList[i]))
+
+st.success(body="Check Done!")
+
+s= ''
+
+for i in live_list:
+    s += ", " + i + "\n"
+
+st.markdown(s)
